816a501435bb8f57b673dd685dcfe01d0d23ab27
Lucas Ribeiro <lucasvribeiro14@gmail.com>
2025-04-09 11:38:30 -0300

    dotenv example, eas and app.json changes, basic types and icons

---
+++ types/example.ts
@@ -0,0 +1,20 @@
+export interface Example {
+  string: string
+  number?: number
+  boolean: boolean
+  array: string[]
+  union: 'example1' | 'example2' | 'example3'
+  unionArray: ('example1' | 'example2' | 'example3')[]
+  record: Record<string, unknown>
+  date: Date
+  tuple: [number, number]
+  object: {
+    prop1: string
+    prop2: number
+    prop3: boolean
+  }
+
+  onFunction: (data: Partial<Example>) => void
+}
+
+export type ExampleType = 'example1' | 'example2' | 'example3'
