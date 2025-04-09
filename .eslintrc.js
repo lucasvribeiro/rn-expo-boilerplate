162e6579bbf67860041f58b9331915025ce8a6da
Lucas Ribeiro <lucasvribeiro14@gmail.com>
2025-04-09 09:01:02 -0300

    (config) final eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   plugins: ['prettier'],
-  extends: ['expo', 'prettier', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['expo', 'eslint:recommended', 'plugin:prettier/recommended'],
   ignorePatterns: ['node_modules', 'android', 'ios', 'app-example']
 }
